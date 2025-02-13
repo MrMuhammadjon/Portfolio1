1445a81719fc6493bb51fcd92f2f6afd86e02cb8
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-02-13 15:46:07 +0500

    soo good
    s

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let parallax3 = document.getElementById('get-parallax-3');
 window.addEventListener('scroll', ()=> {
     let value = window.scrollY;
 
-    parallax1.style.marginTop = value * 1 + 'px';
+    parallax1.style.marginTop = value * 0.5 + 'px';
     parallax2.style.marginTop = value * -2.5 + 'px';
     parallax3.style.marginTop = value * -1.5 + 'px';
 
