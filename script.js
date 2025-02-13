e115a4df565a29dd3aed124a3e696035fe523697
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-02-13 12:33:52 +0500

    i add parallax

---
+++ script.js
@@ -0,0 +1,13 @@
+let parallax1 = document.getElementById('get-parallax-1');
+let parallax2 = document.getElementById('get-parallax-2');
+let parallax3 = document.getElementById('get-parallax-3');
+
+
+window.addEventListener('scroll', ()=> {
+    let value = window.scrollY;
+
+    parallax1.style.marginTop = value * 1 + 'px';
+    parallax2.style.marginTop = value * -2.5 + 'px';
+    parallax3.style.marginTop = value * -1.5 + 'px';
+
+})
